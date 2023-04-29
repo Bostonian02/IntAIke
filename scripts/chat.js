e356f5caf3b0ca6f924ee3b7ddc52ba4b195329a
CJ Cosby <charlescjcosby@gmail.com>
2023-04-29 19:07:09 -0400

    Added user text bubble

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,20 @@
+const USER = true;
+const BOT = false;
+
 function postMessage(){
     const chatBar = document.getElementById('userInput');
-    var message = chatBar.value;
-    alert(message);
+    const chatSection =document.getElementById('chatSection')
+    if(chatBar.value === "")
+        return;
+    var message = makeMessage(chatBar.value, USER);
+    chatSection.innerHTML+=message;
+}
+
+function makeMessage(message, user){
+    if(message === "")
+        return null;
+    var className;
+    user ? className="userMessage" : className="botMessage";
+    var bubble = '<div class="'+className+'">'+message+'</div>';
+    return bubble;
 }
