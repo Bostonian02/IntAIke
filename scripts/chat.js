b3af7d01978fe626265183a629360d9794de3d09
Bostonian02 <bostonian02@gmail.com>
2023-04-29 19:23:49 -0400

    Change postMessage function

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,27 @@
-function postMessage(){
+// function postMessage(){
+//     const chatBar = document.getElementById('userInput');
+//     var message = chatBar.value;
+//     alert(message);
+// }
+const postMessage = async () => {
     const chatBar = document.getElementById('userInput');
     var message = chatBar.value;
-    alert(message);
+
+    var obj = { message: message }
+    var js = JSON.stringify(obj)
+
+    try
+    {
+        var response = await fetch('http://127.0.0.1:5000/api/sendMessage',
+        { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+
+        var res = await response.text();
+
+        console.log(res);
+
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 }
