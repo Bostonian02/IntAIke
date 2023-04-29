c797bf891c08316948f585cb0b7f5f0d2563f9e0
Bostonian02 <bostonian02@gmail.com>
2023-04-29 12:50:44 -0400

    Modify Agent to use multiple tools

--- MonetaryValueBot.py
+++ MonetaryValueBot.py
@@ -1,22 +1,55 @@
-# type: ignore
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+# Imports
+from langchain import LLMMathChain, LLMChain, SQLDatabaseChain
+from langchain.agents import create_sql_agent, ZeroShotAgent, Tool, AgentExecutor
+# from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 from langchain.llms.openai import OpenAI
-from langchain.agents import AgentExecutor
 from dotenv import load_dotenv
 
 # Load API Keys
 load_dotenv()
 
-# Change the path after sqlite: to where you have Cases.db stored on your machine
+# Tools
 my_db = SQLDatabase.from_uri("sqlite:///Cases.db")
-toolkit = SQLDatabaseToolkit(db=my_db, llm=OpenAI(temperature=0))
+db_chain = SQLDatabaseChain(llm=OpenAI(temperature=0), database=my_db, verbose=True)
+llm_math_chain = LLMMathChain(llm=OpenAI(temperature=0), verbose=True)
 
-agent_executor = create_sql_agent(
-    llm=OpenAI(temperature=0),
-    toolkit=toolkit,
-    verbose=True
+# Create our tools
+tools = [
+    Tool(
+        name="SQL",
+        func=db_chain.run,
+        description="Useful for retrieving information from databases"
+    ),
+    Tool(
+        name="Math",
+        func=llm_math_chain.run,
+        description="Useful for when you need to do math operations"
+    ),
+]
+
+# Prefix and suffix
+prefix="Answer the following questions as best you can. You have access to the following tools:"
+suffix="""Begin!
+
+Question: {input}
+{agent_scratchpad}"""
+
+# Create the prompt
+prompt = ZeroShotAgent.create_prompt(
+    tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["input", "agent_scratchpad"]
 )
 
+# Create the chain and agent
+llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+
+tool_names = [tool.name for tool in tools]
+agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names)
+
+agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+
+# Run the agent
 agent_executor.run("Based on the level of compensation stored in the database, estimate how much money could be won from a car accident case")
