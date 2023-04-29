3a3b1261066e83312e8b8ae401d8df8b81ecf79a
Bostonian02 <bostonian02@gmail.com>
2023-04-29 15:30:26 -0400

    Include input.html to route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_cors import CORS
 import sqlite3 as sql
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 CORS(app)
 
 @app.route("/")
