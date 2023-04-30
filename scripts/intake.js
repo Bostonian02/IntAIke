b50abc7d266a7bc8d1d612ea62bc5cea0567f43a
CJ Cosby <charlescjcosby@gmail.com>
2023-04-30 06:56:42 -0400

    Minor styling changes on all pages

--- scripts/intake.js
+++ scripts/intake.js
@@ -1,6 +1,6 @@
 function hideDiv(id){
     var elem=document.getElementById(id);
     var status = getComputedStyle(elem).display;
-    // status == "none" ? elem.style.opacity=1 : elem.style.opacity=0;
+    //= status == "none" ? elem.style.opacity=1 : elem.style.opacity=0;
     status == "none" ? elem.style.display="flex" : elem.style.display="none";
 }
